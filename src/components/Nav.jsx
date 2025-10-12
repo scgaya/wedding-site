77d7183921a97c68a4f09f876a586b97970dd8bb
Sara Gaya <saracgaya@gmail.com>
2025-10-11 21:39:55 -0400

    mobile menu transition

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -33,30 +33,26 @@ const Nav = () => {
                 <div className="Nav-hamburger-menu">
                     <Hamburger toggled={isOpen} toggle={setOpen} />
                 </div>
-                {isOpen && (
-                <>
-                    <nav className='Nav-mobile-menu'>
-                        <NavLink to="/" onClick={() => setOpen(!isOpen)} end>
+                    <nav className={isOpen ? 'Nav-mobile-menu-open' : 'Nav-mobile-menu-closed'}>
+                        <NavLink to="/" onClick={() => setOpen(isOpen => !isOpen)} end>
                             Home
                         </NavLink>
-                        <NavLink to="story" onClick={() => setOpen(!isOpen)} end>
+                        <NavLink to="story" onClick={() => setOpen(isOpen => !isOpen)} end>
                             Our Story
                         </NavLink>
-                        <NavLink to="venue" onClick={() => setOpen(!isOpen)} end>
+                        <NavLink to="venue" onClick={() => setOpen(isOpen => !isOpen)} end>
                             Venue
                         </NavLink>
-                        <NavLink to="itinerary" onClick={() => setOpen(!isOpen)} end>
+                        <NavLink to="itinerary" onClick={() => setOpen(isOpen => !isOpen)} end>
                             Itinerary
                         </NavLink>
-                        <NavLink to="faqs" onClick={() => setOpen(!isOpen)} end>
+                        <NavLink to="faqs" onClick={() => setOpen(isOpen => !isOpen)} end>
                             FAQs
                         </NavLink>
-                        <NavLink to="rsvp" onClick={() => setOpen(!isOpen)} end>
+                        <NavLink to="rsvp" onClick={() => setOpen(isOpen => !isOpen)} end>
                             RSVP
                         </NavLink>
                     </nav>
-                </>
-                )}
             </nav>
         </>
     )
