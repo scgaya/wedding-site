70b0a72175be9d6f06499c5ae62b98dca6edf7fa
Sara Gaya <saracgaya@gmail.com>
2025-10-12 14:56:48 -0400

    story blurb and faq wording change

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -24,7 +24,7 @@ const FAQ = () => {
                 <hr width="90%" size="1" />
                 <div className="FAQ-row">
                     <h2>Getting Around</h2>
-                    <p>There is a considerable distance between the venue for Friday and the venue for Saturday & Sunday, so for guests 
+                    <p>There is a considerable distance between the venue for Friday and the venue for Saturday, so for guests 
                         arriving from out of town we recommend renting a car. Carpooling is also encouraged, and ridesharing is always an option.
                         All venues will have plenty of parking spaces. 
                     </p>
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -10,14 +10,19 @@ const Story = () => {
                 <img src={StoryImage} alt="Our Story pic of Sara smiling with Andrew looking at her" />
                 <div className="Story-paragraph">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Sara and Andrew first met at the University of Florida (Go Gators!) as fellow Computer Science students. Andrew thought Sara was pretty from the start and made an effort 
+                        to spend time with her. After forming a close friendship, they soon realized that they wanted to spend just about all their time together. 
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Andrew would offer Sara rides home during college breaks, and both sides of parents noticed that these two were spending quite a 
+                        bit of time together! But of course they somehow didn't start dating until 2020, right when the COVID pandemic was in full swing. But even during that tumultuous time, they were happy to spend time together however possible. 
                     </p>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        
+                        Fast forward 5 years later, Andrew asked Sara for her hand and she happily said yes! They are ecstatic about becoming husband and wife, 
+                        and can't wait to celebrate with all of you in May.
                     </p>
+                    
                 </div>
             </div>
         </div>
