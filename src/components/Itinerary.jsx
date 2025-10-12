bb08ebcaa1f9d9b2fd4ff9738d040f219e477dd4
Sara Gaya <saracgaya@gmail.com>
2025-10-12 14:08:25 -0400

    itinerary re-wording

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -8,13 +8,13 @@ const Itinerary = () => {
         <div className="Title">Itinerary</div>
         <div className="Itinerary-details">
             <div className="Itinerary-timeline">
-                <h5>Day 1: Arrival</h5>
-                <p>Check-in, unwind, and explore the South Florida area at your own pace. This is your day to take it easy before all the festivities begin!</p>
-                <h5>Day 2: The Wedding</h5>
+                <h5>Week of Wedding: Arrival</h5>
+                <p>Check-in, unwind, and explore the South Florida area at your own pace. This is your chance to relax before all the festivities begin!</p>
+                <h5>Fri, May 22: The Wedding</h5>
                 <p>The ceremony begins at 5 p.m., with dinner, drinks, and dancing to follow. </p>
-                <h5>Day 3: The Pakistani Reception</h5>
+                <h5>Sat, May 23: The Pakistani Reception</h5>
                 <p>Cocktails & hors-d'ouevres begin at 5 p.m., with the reception to follow.</p>
-                <h5>Day 4: Farewell Brunch</h5>
+                <h5>Sun, May 24: Farewell Brunch</h5>
                 <p>A farewell brunch will be held at The Club at Weston Hills beginning at 8 a.m.. One last hurrah before our goodbyes!</p>
             </div>
             <img src={ItineraryImage} alt="Andrew & Sara smiling together in the Boston Public Garden" />
