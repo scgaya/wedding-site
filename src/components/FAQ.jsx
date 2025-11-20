48d41fbeb0ad8fa83b40b21ac458bc31536f9af0
Sara Gaya <saracgaya@gmail.com>
2025-11-20 17:53:51 -0500

    faq changes

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -16,9 +16,7 @@ const FAQ = () => {
                 <hr width="90%" size="1" />
                 <div className="FAQ-row">
                     <h2>Accommodations</h2>
-                    <p>We've reserved a block of rooms at X hotel in Weston, FL. When you make a reservation,
-                        let the hotel know you are part of the Gaya-Sanchez wedding to receive the discount. You are 
-                        also welcome to stay anywhere in the Miami/Ft. Lauderdale area!
+                    <p>You are welcome to stay anywhere in the Miami/Ft. Lauderdale area! We will update this section with recommended hotels in the near future.
                     </p>
                 </div>
                 <hr width="90%" size="1" />
@@ -33,10 +31,10 @@ const FAQ = () => {
                 <div className="FAQ-row">
                     <h2>Gift Registry</h2>
                     <p>Your presence is truly the best gift we could ask for, nothing more needed! But if you would like to 
-                        contribute to our new chapter together, we've put together a honeymoon fund as well as a small registry with 
-                        some of our most wished-for items. 
-                        Link to fund: <NavLink to="#" end>www.saraandandrewfund.com</NavLink>. 
-                        Link to registry: <NavLink to="#" end>www.saraandandrewregistry.com</NavLink>
+                        contribute to our new chapter together, we will be putting together a honeymoon fund as well as a small registry with 
+                        some of our most wished-for items. Stay tuned!
+                        {/* Link to fund: <NavLink to="#" end>www.saraandandrewfund.com</NavLink>. 
+                        Link to registry: <NavLink to="#" end>www.saraandandrewregistry.com</NavLink> */}
                     </p>
                 </div>
             </div>
