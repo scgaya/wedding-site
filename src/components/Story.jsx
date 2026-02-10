341a73600f4ed63099d4453b849fe855361813f2
Sara Gaya <saracgaya@gmail.com>
2026-02-09 21:25:28 -0500

    story edit

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -11,14 +11,14 @@ const Story = () => {
                 <div className="Story-paragraph">
                     <p>
                         Sara and Andrew first met at the University of Florida (Go Gators!) as fellow Computer Science students. Andrew thought Sara was pretty from the start and made an effort 
-                        to spend time with her. After forming a close friendship, they soon realized that they wanted to spend just about all their time together. 
+                        to get to know her. After forming a close friendship, they soon realized that they wanted to spend just about all their time together. 
                     </p>
                     <p>
-                        Andrew would offer Sara rides home during college breaks, and both sides of parents noticed that these two were spending quite a 
-                        bit of time together! But they didn't start dating until 2020, right when the COVID pandemic hit. But even during that tumultuous time, they were happy to spend time together however possible. 
+                        Andrew would offer Sara rides home during school breaks, and both sides of parents noticed that these two were spending quite a 
+                        bit of time with each other! But they didn't start dating until 2020, right when the COVID pandemic hit. Yet even during that tumultuous time, 
+                        they were happy to be in each other's presence. 
                     </p>
                     <p>
-                        
                         Fast forward 5 years later, Andrew asked Sara for her hand and she happily said yes! They are ecstatic about becoming husband and wife, 
                         and can't wait to celebrate with all of you in May.
                     </p>
